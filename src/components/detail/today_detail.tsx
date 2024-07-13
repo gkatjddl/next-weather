61125358e1e0a487a4752a011738ca11c406ab59
gkatjddl <106408479+gkatjddl@users.noreply.github.com>
2024-07-14 07:19:04 +0900

    Update today_detail.tsx

--- src/components/detail/today_detail.tsx
+++ src/components/detail/today_detail.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './today_detail.css'
+import './today_detail.css';
 import { LuEye, LuSunrise, LuSunset } from 'react-icons/lu';
 import { FiDroplet } from 'react-icons/fi';
 import { MdAir } from 'react-icons/md';
@@ -9,14 +9,14 @@ export interface TodayDetailProps{
     visibility: string;
     humidity: string;
     windSpeed: string;
-    airPressure: string;
+    airPressure : string;
     sunrise : string;
     sunset : string;
 }
 
-export interface SingleWeatherDtail{
-    information: string;
-    icon:React.ReactNode;
+export interface SingleWeatherDetailProps{
+    information: string;            // 날씨 정보(텍스트)
+    icon:React.ReactNode;           // 날씨 정보(이미지명)
     value:string;
 }
 
@@ -24,21 +24,28 @@ export default function TodayDetail(props : TodayDetailProps){
     return(
         <>
             <SingleWeatherDetail information='가시성' icon={<LuEye/>} value={props.visibility}/>
+
             <SingleWeatherDetail information='습도' icon={<FiDroplet/>} value={props.humidity}/>
+
             <SingleWeatherDetail information='풍속' icon={<MdAir/>} value={props.windSpeed}/>
+
             <SingleWeatherDetail information='기압' icon={<ImMeter/>} value={props.airPressure}/>
-            <SingleWeatherDetail information='일출시간' icon={<LuSunrise/>} value={props.sunrise}/>
-            <SingleWeatherDetail information='일몰시간' icon={<LuSunset/>} value={props.sunset}/>
+
+            <SingleWeatherDetail information='일출 시간' icon={<LuSunrise/>} value={props.sunrise}/>
+
+            <SingleWeatherDetail information='일몰 시간' icon={<LuSunset/>} value={props.sunset}/>
         </>
     )
 }
 
-function SingleWeatherDetail(props: SingleWeatherDtailProps){
+function SingleWeatherDetail(props: SingleWeatherDetailProps){
     return(
         <div className='todaydetail-item'>
-            <p className=''>
+            <p className='todaydetail-info-text'>
                 {props.information}
             </p>
+            <div className='todaydetail-icon-size'>{props.icon}</div>
+            <p>{props.value}</p>
         </div>
     )
 }
